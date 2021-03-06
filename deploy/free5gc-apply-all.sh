793f83f23d6d8bc480c895e8aa61496a52b8876e
p76081158 <p76081158@gs.ncku.edu.tw>
2021-03-06 23:41:32 +0800

    auto kubectl apply & delete script

---
+++ deploy/free5gc-apply-all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+checkpod () {
+while [[ $(kubectl -n free5gc get pods -l app=$1 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" $1 && sleep 1; done
+}
+
+kubectl apply -f free5gc-namespace.yaml && sleep 1
+cd mongodb && kubectl apply -k . && cd ..
+
+checkpod free5gc-mongodb
+cd nrf     && kubectl apply -k . && cd ..
+cd upf-1   && kubectl apply -k . && cd ..
+cd upf-2   && kubectl apply -k . && cd ..
+
+checkpod free5gc-nrf
+checkpod free5gc-upf-1
+checkpod free5gc-upf-2
+cd amf     && kubectl apply -k . && cd ..
+cd ausf    && kubectl apply -k . && cd ..
+cd smf     && kubectl apply -k . && cd ..
+cd nssf    && kubectl apply -k . && cd ..
+cd pcf     && kubectl apply -k . && cd ..
+cd udm     && kubectl apply -k . && cd ..
+cd udr     && kubectl apply -k . && cd ..
+
---
+++ deploy/free5gc-delete-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd mongodb && kubectl delete -k . && cd ..
+cd nrf     && kubectl delete -k . && cd ..
+cd upf-1   && kubectl delete -k . && cd ..
+cd upf-2   && kubectl delete -k . && cd ..
+cd amf     && kubectl delete -k . && cd ..
+cd ausf    && kubectl delete -k . && cd ..
+cd smf     && kubectl delete -k . && cd ..
+cd nssf    && kubectl delete -k . && cd ..
+cd pcf     && kubectl delete -k . && cd ..
+cd udm     && kubectl delete -k . && cd ..
+cd udr     && kubectl delete -k . && cd ..
+kubectl delete -f free5gc-namespace.yaml
