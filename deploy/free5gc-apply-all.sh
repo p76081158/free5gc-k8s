153435d201edead940b113ff245f6b2b77695c77
p76081158 <p76081158@gs.ncku.edu.tw>
2021-03-08 21:38:53 +0800

    modify shell script

--- deploy/free5gc-apply-all.sh
+++ deploy/free5gc-apply-all.sh
@@ -4,25 +4,28 @@ checkpod () {
 while [[ $(kubectl -n free5gc get pods -l app=$1 -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod" $1 && sleep 1; done
 }
 
+kustomizeapply () {
+cd $1 && kubectl apply -k . && cd ../../
+}
+
 kubectl apply -f free5gc-namespace.yaml && sleep 1
-cd mongodb && kubectl apply -k . && cd ..
+cd mongodb && kustomizeapply base
 
 # Depends on mongodb
 checkpod free5gc-mongodb
-cd nrf     && kubectl apply -k . && cd ..
-cd upf-1   && kubectl apply -k . && cd ..
-cd upf-2   && kubectl apply -k . && cd ..
-cd webui   && kubectl apply -k . && cd ..
+cd nrf     && kustomizeapply base
+cd upf-1   && kustomizeapply base
+cd upf-2   && kustomizeapply base
+cd webui   && kustomizeapply base
 
 # Depends on nrf & upf
 checkpod free5gc-nrf
 checkpod free5gc-upf-1
 checkpod free5gc-upf-2
-cd amf     && kubectl apply -k . && cd ..
-cd ausf    && kubectl apply -k . && cd ..
-cd smf     && kubectl apply -k . && cd ..
-cd nssf    && kubectl apply -k . && cd ..
-cd pcf     && kubectl apply -k . && cd ..
-cd udm     && kubectl apply -k . && cd ..
-cd udr     && kubectl apply -k . && cd ..
-
+cd amf     && kustomizeapply base
+cd ausf    && kustomizeapply base
+cd smf     && kustomizeapply base
+cd nssf    && kustomizeapply base
+cd pcf     && kustomizeapply base
+cd udm     && kustomizeapply base
+cd udr     && kustomizeapply base
