e5f1157f8f00246dbedeb7242060bafc411fbde3
p76081158 <p76081158@gs.ncku.edu.tw>
2021-03-31 15:24:02 +0800

    modify shell script

--- deploy/free5gc-apply-all.sh
+++ deploy/free5gc-apply-all.sh
@@ -10,24 +10,26 @@ cd ../../
 }
 
 kubectl apply -f free5gc-namespace.yaml && sleep 1
-cd mongodb && kustomizeapply base
-cd gnbsim  && kustomizeapply overlays
+cd mongodb      && kustomizeapply base
+# cd gnbsim     && kustomizeapply overlays
+cd UERANSIM-gnb && kustomizeapply base
+cd UERANSIM-ue  && kustomizeapply base
 
 # Depends on mongodb
 checkpod free5gc-mongodb
-cd nrf     && kustomizeapply base
-cd upf-1   && kustomizeapply overlays
-# cd upf-2   && kustomizeapply overlays
-cd webui   && kustomizeapply base
+cd nrf          && kustomizeapply base
+cd upf-1        && kustomizeapply overlays
+# cd upf-2      && kustomizeapply overlays
+cd webui        && kustomizeapply base
 
 # Depends on nrf & upf
 checkpod free5gc-nrf
 checkpod free5gc-upf-1
 # checkpod free5gc-upf-2
-cd amf     && kustomizeapply overlays
-cd ausf    && kustomizeapply base
-cd smf     && kustomizeapply overlays
-cd nssf    && kustomizeapply base
-cd pcf     && kustomizeapply base
-cd udm     && kustomizeapply base
-cd udr     && kustomizeapply base
+cd amf          && kustomizeapply overlays
+cd ausf         && kustomizeapply base
+cd smf          && kustomizeapply overlays
+cd nssf         && kustomizeapply base
+cd pcf          && kustomizeapply base
+cd udm          && kustomizeapply base
+cd udr          && kustomizeapply base
--- deploy/free5gc-delete-all.sh
+++ deploy/free5gc-delete-all.sh
@@ -5,17 +5,19 @@ cd $1 && kubectl delete -k .
 cd ../../
 }
 
-cd mongodb && kustomizedelete base
-cd gnbsim  && kustomizedelete overlays
-cd nrf     && kustomizedelete base
-cd upf-1   && kustomizedelete overlays
-# cd upf-2   && kustomizedelete overlays
-cd webui   && kustomizedelete base
-cd amf     && kustomizedelete overlays
-cd ausf    && kustomizedelete base
-cd smf     && kustomizedelete overlays
-cd nssf    && kustomizedelete base
-cd pcf     && kustomizedelete base
-cd udm     && kustomizedelete base
-cd udr     && kustomizedelete base
+cd mongodb      && kustomizedelete base
+# cd gnbsim     && kustomizedelete overlays
+cd UERANSIM-gnb && kustomizedelete base
+cd UERANSIM-ue  && kustomizedelete base
+cd nrf          && kustomizedelete base
+cd upf-1        && kustomizedelete overlays
+# cd upf-2      && kustomizedelete overlays
+cd webui        && kustomizedelete base
+cd amf          && kustomizedelete overlays
+cd ausf         && kustomizedelete base
+cd smf          && kustomizedelete overlays
+cd nssf         && kustomizedelete base
+cd pcf          && kustomizedelete base
+cd udm          && kustomizedelete base
+cd udr          && kustomizedelete base
 kubectl delete -f free5gc-namespace.yaml
